8a1a3571f1e632a924f3b166cc9743e0cbb6c776
zhengpanone <zhengpanone@hotmail.com>
2023-04-11 22:27:10 +0800

    nacos作为配置中心

--- gmall-coupon/src/main/java/com/zp/coupon/controller/TestController.java
+++ gmall-coupon/src/main/java/com/zp/coupon/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.zp.coupon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * TestController
@@ -11,8 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Autowired
+    private Environment env;
     @GetMapping("/")
     public String test(){
         return "gmall-coupon";
+    }
+    @GetMapping("/config")
+    public Map<String,String>  getAllEnv(){
+        Map<String, String> configs = new HashMap<>();
+        MutablePropertySources propertySources = ((AbstractEnvironment) env).getPropertySources();
+        for(PropertySource<?> propertySource: propertySources){
+            if(propertySource instanceof EnumerablePropertySource){
+                for(String name:  ((EnumerablePropertySource<?>) propertySource).getPropertyNames()){
+                    if (name != null ) {
+                        configs.put(name, env.getProperty(name));
+                    }
+                }
+            }
+        }
+        return configs;
     }
 }
