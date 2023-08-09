962320cb292678d5500f5bbf75c4c00b234a1ae1
zhengpanone <zhengpanone@hotmail.com>
2023-08-09 08:14:51 +0800

    前端采用vue-next-admin

--- gmall-ui/src/main.ts
+++ gmall-ui/src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import pinia from '/@/stores/index';
+import App from '/@/App.vue';
+import router from '/@/router';
+import { directive } from '/@/directive/index';
+import { i18n } from '/@/i18n/index';
+import other from '/@/utils/other';
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus';
+import '/@/theme/index.scss';
+import VueGridLayout from 'vue-grid-layout';
+
+const app = createApp(App);
+
+directive(app);
+other.elSvg(app);
+
+app.use(pinia).use(router).use(ElementPlus).use(i18n).use(VueGridLayout).mount('#app');
