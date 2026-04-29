db2731f2d710d99098f088a32987176dcb65d25d
zhengpanone <zhengpanone@hotmial.com>
2026-04-30 00:34:11 +0800

    feature: 商城统计服务

---
+++ backend/gmall-module-mall/gmall-module-statistics-server/src/main/java/com/zp/gmall/module/statistics/StatisticsApplication.java
@@ -0,0 +1,17 @@
+package com.zp.gmall.module.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : zhengpanone
+ * Date : 2026/4/30 00:26
+ * Version : v1.0.0
+ * Description:
+ */
+@SpringBootApplication
+public class StatisticsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticsApplication.class, args);
+    }
+}
