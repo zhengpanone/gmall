c8c63b9ca1e7ff69379bb9782ee69595a14bb49a
zhengpanone <zhengpanone@hotmail.com>
2026-04-30 09:20:26 +0800

    feature: report项目

---
+++ backend/gmall-module-report/gmall-module-report-api/src/main/java/com/zp/gmall/module/report/constant/ErrorCodeConstants.java
@@ -0,0 +1,8 @@
+package com.zp.gmall.module.report.constant;
+
+import com.zp.gmall.framework.common.exception.ErrorCode;
+
+public interface ErrorCodeConstants {
+    // ========== GoView 模块 1-003-000-000 ==========
+    ErrorCode GO_VIEW_PROJECT_NOT_EXISTS = new ErrorCode(1_003_000_000, "GoView 项目不存在");
+}
--- backend/gmall-module-report/gmall-module-report-server/src/main/java/com/zp/gmall/module/report/ReportServerApplication.java
+++ backend/gmall-module-report/gmall-module-report-server/src/main/java/com/zp/gmall/module/report/ReportServerApplication.java
@@ -1,4 +1,4 @@
-package com.zp.module.report;
+package com.zp.gmall.module.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
